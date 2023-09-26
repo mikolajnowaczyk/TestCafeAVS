2fec5f4ed0312b15840b25c6f549ca4686898e06
Mikolaj <mikolaj35@wp.pl>
2023-09-26 12:02:53 +0200

    Testcafe config

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,4 +1,33 @@
 module.exports = {
-  src: "tests",
+  src: ["e2e/tests"],
   baseUrl: "https://angular-qa-recruitment-app.netlify.app/",
+  browsers: ["chrome", "firefox", "edge"],
+  screenshoots: {
+    path: "e2e/artifacts/screenshots/",
+    fullPage: false,
+    thumbnails: false,
+    takeOnfails: true,
+  },
+  videoPath: "e2e/artifacts/videos",
+  videoOptions: {
+    singleFile: false,
+    failedOnly: true,
+  },
+  videoEncodingOptions: {
+    framerate: 120,
+    aspect: "16:9",
+  },
+  pageLoadTimeout: 60000,
+  selectorTimeout: 200,
+  pageNavigateAttempts: 3,
+  pageLoadTimeout: 60000,
+  concurrency: 1,
+  speed: 1,
+  cache: false,
+  debugMode: false,
+  stopOnFirstfail: false,
+  // quarantineMode: {
+  // attemptLimit: 1,
+  // successThreshold: 1,
+  // },
 };
